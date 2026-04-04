b69d8a42e8b3cab3cf6bf6e6a842d05703df6a8f
abhishek-031-k <abhishkumar159@gmail.com>
2026-04-04 23:26:21 +0530

    Refactor Cloudinary config into a separate function
    Encapsulate Cloudinary configuration in a function.

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -1,10 +1,14 @@
 import { v2 as cloudinary } from "cloudinary";
 import { ENV } from "./env.js";
 
-cloudinary.config({
-  cloud_name: ENV.CLOUDINARY_CLOUD_NAME || process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: ENV.CLOUDINARY_API_KEY || process.env.CLOUDINARY_API_KEY,
-  api_secret: ENV.CLOUDINARY_API_SECRET || process.env.CLOUDINARY_API_SECRET,
-});
+const configureCloudinary = () => {
+  cloudinary.config({
+    cloud_name: ENV.CLOUDINARY_CLOUD_NAME || process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: ENV.CLOUDINARY_API_KEY || process.env.CLOUDINARY_API_KEY,
+    api_secret: ENV.CLOUDINARY_API_SECRET || process.env.CLOUDINARY_API_SECRET,
+  });
+};
+
+configureCloudinary();
 
 export default cloudinary;
