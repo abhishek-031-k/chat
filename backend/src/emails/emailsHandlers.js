fb2af93b899fe564e9fa9fbcc2f8ff48df345aca
abhishek-031-k <abhishkumar159@gmail.com>
2026-04-06 16:17:46 +0530

    Update emailsHandlers.js

--- backend/src/emails/emailsHandlers.js
+++ backend/src/emails/emailsHandlers.js
@@ -5,25 +5,23 @@ import { ENV } from "../lib/env.js";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: ENV.GMAIL_USER, 
-    pass: ENV.GMAIL_APP_PASSWORD, 
+    user: ENV.GMAIL_USER,
+    pass: ENV.GMAIL_APP_PASSWORD,
   },
 });
 
- */
 export const sendWelcomeEmail = async (email, name, clientURL) => {
   try {
     const mailOptions = {
-      from: `Chatify <${ENV.GMAIL_USER}>`, 
-      to: email, 
+      from: `Chatify <${ENV.GMAIL_USER}>`,
+      to: email,
       subject: "Welcome to Chatify!",
-      html: createWelcomeEmailTemplate(name, clientURL), 
+      html: createWelcomeEmailTemplate(name, clientURL),
     };
 
     const info = await transporter.sendMail(mailOptions);
-    console.log("Welcome Email sent successfully via Gmail:", info.messageId);
-    
+    console.log("Welcome Email sent successfully:", info.messageId);
   } catch (error) {
-    console.error("Error sending welcome email via Gmail:", error);
+    console.error("Error sending welcome email:", error);
   }
 };
