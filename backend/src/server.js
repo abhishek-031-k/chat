0391c258e5d7574683a2794d65a4249a5b676d59
abhishek-031-k <abhishkumar159@gmail.com>
2026-06-09 12:15:04 +0530

    Update allowedOrigins in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ import { app, server } from "./lib/socket.js";
 const PORT = ENV.PORT || 3000;
 
 const allowedOrigins = [
-  "https://chat-nt1m1nbag-abhishek-031-ks-projects.vercel.app/login",                    
+  "https://chat-nt1m1nbag-abhishek-031-ks-projects.vercel.app",                    
   "http://localhost:5173",                
   "http://localhost:3000"                   
 ];
