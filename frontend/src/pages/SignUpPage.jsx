c7bda81041aecace980f85fe3f20a91010d6ef63
abhishek-031-k <abhishkumar159@gmail.com>
2026-06-10 23:53:42 +0530

    Update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -44,7 +44,7 @@ function SignUpPage() {
                         value={formData.fullName}
                         onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
                         className="input"
-                        placeholder="John Doe"
+                        placeholder="Full Name"
                         required
                       />
                     </div>
@@ -60,7 +60,7 @@ function SignUpPage() {
                         value={formData.email}
                         onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                         className="input"
-                        placeholder="johndoe@gmail.com"
+                        placeholder="abc@gmail.com"
                         required
                       />
                     </div>
